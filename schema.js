b657fb015848ff2887d9a2d1b6688b2e8ff2b4e3
chisimdi <chisimdi.ezeanieto@nayalabs.co>
2021-09-23 18:21:50 +0100

    Done with creating schema.

--- schema.js
+++ schema.js
@@ -1,13 +1,53 @@
 import { buildSchema } from "graphql";
 
-export default buildSchema(`
+const schema = buildSchema(`
   type Course {
     id: ID
     name: String
     category: String
-    price: String
+    price: Int
     language: String
+    stack: Stack
     email: String
-    teachingAssistants: [TeachingAssistants]
+    teachingAssistants: [TeachingAssistant]
+  }
+
+  type TeachingAssistant {
+    firstName: String
+    lastName: String
+    experience: Int
+  }
+
+  enum Stack {
+    WEB
+    MOBILE
+    OTHER
+  }
+
+  type Query {
+    getCourse(id: ID): Course
+  }
+
+  input CourseInput {
+    id: ID
+    name: String!
+    category: String
+    price: Int!
+    language: String
+    stack: Stack
+    email: String
+    teachingAssistants: [TeachingAssistantInput]!
+  }
+
+  input TeachingAssistantInput {
+    firstName: String
+    lastName: String
+    experience: Int
+  }
+
+  type Mutation {
+    createCourse(input: CourseInput): Course
   }
 `);
+
+export default schema;
